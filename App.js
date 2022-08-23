e3946e722812d857132727ed20c8f9962373e52a
Ragnar-S-F <ragnar.susi@fraktio.fi>
2022-08-23 11:38:40 +0300

    Testi committi

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Iphone toimii! Jee jee ninnu </Text>
+      <Text>Testi </Text>
       <StatusBar style="auto" />
     </View>
   );
