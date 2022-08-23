00f95410260d23f332cc85d64e28337ec1f090c6
Ragnar-S-F <ragnar.susi@fraktio.fi>
2022-08-23 12:39:52 +0300

    Ensimmäinen osio tehty

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testi </Text>
+      <Text>Testir </Text>
       <StatusBar style="auto" />
     </View>
   );
