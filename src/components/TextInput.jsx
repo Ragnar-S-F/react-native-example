0466772d632693b117d350f8a372f2fc72eb2611
Ragnar-S-F <ragnar.susi@fraktio.fi>
2022-08-24 17:06:34 +0300

    Form validation valmis

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,6 +1,7 @@
 import { Formik } from 'formik';
-import { TextInput as NativeTextInput, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { TextInput as NativeTextInput, StyleSheet, View, Button } from 'react-native';
 import Text from './Text';
+import * as yup from 'yup';
 
 const styles = StyleSheet.create({
     input: {
@@ -8,44 +9,60 @@ const styles = StyleSheet.create({
         margin: 12,
         borderWidth: 1,
         padding: 10,
+        borderRadius: 8
       },
-      loginText:{
-        color:'#fff',
-        textAlign:'center',
-        paddingLeft : 10,
-        paddingRight : 10
-    },
-      loginScreenButton:{
-        marginRight:40,
-        marginLeft:40,
-        marginTop:10,
-        paddingTop:10,
-        paddingBottom:10,
-        backgroundColor:'#2252d6',
-        borderRadius:10,
+      error: {
+        height: 40,
+        margin: 12,
         borderWidth: 1,
-        borderColor: '#fff',
-        textAlign: "center"
+        padding: 10,
+        borderRadius: 8,
+        borderColor:"#d73a4a"
+      },
+      loginScreenButton:{
+        marginTop:40,
+        marginRight:30,
+        marginLeft:30,
+        backgroundColor:'#2252d6',
+        borderRadius:8,
       },
 });
 
-const TextInput = ({ ...props }) => {
+const validationSchema = yup.object().shape({
+    username: yup
+      .string()
+      .required('Username is required'),
+    password: yup
+      .string()
+      .required('Password is required'),
+  });
+
+const TextInput = () => {
   return (
     <>
     <Formik
      initialValues={{ username: "", password: "" }}
      onSubmit={values => console.log(values)}
+     validationSchema={validationSchema}
    >
-    {({ handleChange, handleSubmit, values }) => (
+    {({ handleChange, handleSubmit, values, isValid, errors, touched }) => (
         <View>
-            <NativeTextInput style={styles.input} {...props} placeholder="Username" value={values.username} onChangeText={handleChange('username')}/>
-            <NativeTextInput style={styles.input} {...props} secureTextEntry placeholder="Password" value={values.password} onChangeText={handleChange('password')}/>
-            <TouchableOpacity
-                style={styles.loginScreenButton}
+            <NativeTextInput style={!errors.username ? styles.input : styles.error} placeholder="Username" value={values.username} onChangeText={handleChange('username')}/>
+            {errors.username && touched.username ? (
+             <View paddingLeft={20}><Text style={{color:"#d73a4a"}} >{errors.username}</Text></View>
+           ) : null}
+            <NativeTextInput style={!errors.password ? styles.input : styles.error} secureTextEntry placeholder="Password" value={values.password} onChangeText={handleChange('password')}/>
+            {errors.password && touched.password ? (
+             <View paddingLeft={20}><Text style={{color:"#d73a4a"}}>{errors.password}</Text></View>
+           ) : null}
+           <View style={styles.loginScreenButton}>
+            <Button
                 onPress={handleSubmit}
-                underlayColor='#fff'>
-                    <Text style={styles.loginText}>Login</Text>
-            </TouchableOpacity>
+                disabled={!isValid}
+                title='Login'
+                color={"white"}
+                />
+                </View>
         </View>
   )}
   </Formik>
